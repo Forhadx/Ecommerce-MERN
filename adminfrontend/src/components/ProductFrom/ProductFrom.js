437324055fc6e9945b346065197a28d85d02ed11
forhadx <forhadsh1@gmail.com>
2022-08-28 21:41:21 +0600

    fix issue

--- adminfrontend/src/components/ProductFrom/ProductFrom.js
+++ adminfrontend/src/components/ProductFrom/ProductFrom.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import * as actions from "../../store/actions/index";
-
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as Yup from "yup";
@@ -11,231 +10,219 @@ import { categoryList } from "../../Data/category";
 import "./ProductsFrom.scss";
 
 const ProductFrom = (props) => {
-    const history = useHistory();
+  const history = useHistory();
 
-    const { singleProd, onFetchAllProducts } = props;
+  const { singleProd, onFetchAllProducts } = props;
 
-    const [goBack, setGoBack] = useState(false);
-    const [copySubCate, setCopySubCate] = useState([]);
+  const [goBack, setGoBack] = useState(false);
+  const [copySubCate, setCopySubCate] = useState([]);
 
-    const validationSchema = Yup.object().shape({
-        mainCategory: Yup.string().required("must select one"),
-        subCategory: Yup.string().required(
-            "must select one, before select main category"
-        ),
-        image: singleProd
-            ? Yup.mixed()
-            : Yup.mixed().test(
-                  "file",
-                  "Image is required (minimum 5MB)",
-                  (value) => {
-                      if (!value.length) {
-                          return false;
-                      }
-                      return value[0].size <= 5000000;
-                  }
-              ),
-        name: Yup.string()
-            .required("name is required")
-            .min(3, "too small name, minimum 3 character")
-            .max(60, "too big name, maximum 60 character "),
-        amount: Yup.string()
-            .required("amount is required")
-            .max(20, "maximum 20 character "),
-        price: Yup.number()
-            .required("price is required")
-            .typeError("price is required")
-            .min(10, "minimum price 10৳")
-            .max(2000, "maximum price 2000৳"),
-        brand: Yup.string().max(50, "maximum 50 character "),
-        description: Yup.string().max(500, "maximum 500 character "),
+  const validationSchema = Yup.object().shape({
+    mainCategory: Yup.string().required("must select one"),
+    subCategory: Yup.string().required(
+      "must select one, before select main category"
+    ),
+    image: singleProd
+      ? Yup.mixed()
+      : Yup.mixed().test("file", "Image is required (minimum 5MB)", (value) => {
+          if (!value.length) {
+            return false;
+          }
+          return value[0].size <= 5000000;
+        }),
+    name: Yup.string()
+      .required("name is required")
+      .min(3, "too small name, minimum 3 character")
+      .max(60, "too big name, maximum 60 character "),
+    amount: Yup.string()
+      .required("amount is required")
+      .max(20, "maximum 20 character "),
+    price: Yup.number()
+      .required("price is required")
+      .typeError("price is required")
+      .min(10, "minimum price 10৳")
+      .max(2000, "maximum price 2000৳"),
+    brand: Yup.string().max(50, "maximum 50 character "),
+    description: Yup.string().max(500, "maximum 500 character "),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    reset,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(validationSchema),
+  });
+
+  const mainCategoryHandler = (e) => {
+    categoryList.forEach((c) => {
+      if (c.mCat === e.target.value) {
+        setCopySubCate([...c.sCat]);
+      }
     });
+  };
 
-    const {
-        register,
-        handleSubmit,
-        setValue,
-        reset,
-        formState: { errors },
-    } = useForm({
-        resolver: yupResolver(validationSchema),
-    });
+  useEffect(() => {
+    if (singleProd) {
+      setValue("mainCategory", singleProd.mainCategory);
+      setValue("name", singleProd.name);
+      setValue("amount", singleProd.amount);
+      setValue("price", singleProd.price);
+      setValue("brand", singleProd.brand);
+      setValue("description", singleProd.description);
 
-    const mainCategoryHandler = (e) => {
-        categoryList.forEach((c) => {
-            if (c.mCat === e.target.value) {
-                setCopySubCate([...c.sCat]);
-            }
-        });
-    };
-
-    useEffect(() => {
-        if (singleProd) {
-            setValue("mainCategory", singleProd.mainCategory);
-            setValue("name", singleProd.name);
-            setValue("amount", singleProd.amount);
-            setValue("price", singleProd.price);
-            setValue("brand", singleProd.brand);
-            setValue("description", singleProd.description);
-
-            categoryList.forEach((c) => {
-                if (c.mCat === singleProd.mainCategory) {
-                    setCopySubCate([...c.sCat]);
-                }
-            });
+      categoryList.forEach((c) => {
+        if (c.mCat === singleProd.mainCategory) {
+          setCopySubCate([...c.sCat]);
         }
-        if (goBack) {
-            setGoBack(false);
-            onFetchAllProducts();
-            history.push("/products");
-        }
-    }, [singleProd, setValue, goBack, history, onFetchAllProducts]);
+      });
+    }
+    if (goBack) {
+      setGoBack(false);
+      onFetchAllProducts();
+      history.push("/products");
+    }
+  }, [singleProd, setValue, goBack, history, onFetchAllProducts]);
+
+  if (singleProd) {
+    setValue("subCategory", singleProd.subCategory);
+  }
+
+  const formSubmitHandler = (data) => {
+    const formData = new FormData();
+    formData.append("mainCategory", data.mainCategory);
+    formData.append("subCategory", data.subCategory);
+    formData.append("name", data.name);
+    formData.append("price", data.price);
+    formData.append("amount", data.amount);
+    formData.append("brand", data.brand);
+    formData.append("description", data.description);
 
     if (singleProd) {
-        setValue("subCategory", singleProd.subCategory);
+      if (data.image[0]) {
+        formData.append("image", data.image[0]);
+      } else {
+        formData.append("image", singleProd.image);
+      }
+      // props.onUpdateProduct(singleProd._id, formData);  // stop update porduct
+      setGoBack(true);
+    } else {
+      formData.append("image", data.image[0]);
+      // props.onAddProduct(formData); // stop update porduct
+      reset();
     }
+  };
 
-    const formSubmitHandler = (data) => {
-        const formData = new FormData();
-        formData.append("mainCategory", data.mainCategory);
-        formData.append("subCategory", data.subCategory);
-        formData.append("name", data.name);
-        formData.append("price", data.price);
-        formData.append("amount", data.amount);
-        formData.append("brand", data.brand);
-        formData.append("description", data.description);
-
-        if (singleProd) {
-            if (data.image[0]) {
-                formData.append("image", data.image[0]);
-            } else {
-                formData.append("image", singleProd.image);
-            }
-            // props.onUpdateProduct(singleProd._id, formData);  // stop update porduct
-            setGoBack(true);
-        } else {
-            formData.append("image", data.image[0]);
-            // props.onAddProduct(formData); // stop update porduct
-            reset();
-        }
-    };
-
-    return (
-        <div className="add__product">
-            <Link to="/products" className="add__product--back">
-                &#8592;Back
-            </Link>
-            <h1 className="add__product--header">{props.headline}</h1>
-            <form
-                className="add__product--form"
-                onSubmit={handleSubmit(formSubmitHandler)}
-            >
-                <div className="add__product--form-input">
-                    <label>Main Category</label>
-                    <select
-                        {...register("mainCategory")}
-                        onChange={mainCategoryHandler}
-                    >
-                        <option value="" hidden>
-                            - select one -
-                        </option>
-                        {categoryList.map((c) => (
-                            <option key={c.mCat} value={c.mCat}>
-                                {c.mCat}
-                            </option>
-                        ))}
-                    </select>
-                    <p>{errors.mainCategory?.message}</p>
-                </div>
-                <div className="add__product--form-input">
-                    <label>Sub Category</label>
-                    <select {...register("subCategory")}>
-                        {singleProd ? (
-                            <option value={singleProd.subCategory} hidden>
-                                {singleProd.subCategory}
-                            </option>
-                        ) : (
-                            <option value="" hidden>
-                                - select one -
-                            </option>
-                        )}
-                        {copySubCate.map((s) => (
-                            <option key={s} value={s}>
-                                {s}
-                            </option>
-                        ))}
-                    </select>
-                    <p>{errors.subCategory?.message}</p>
-                </div>
-                <div className="add__product--form-input">
-                    <label>Image</label>
-                    <input
-                        type="file"
-                        accept="image/*"
-                        {...register("image")}
-                    />
-                    <p>{errors.image?.message}</p>
-                </div>
-                <div className="add__product--form-input">
-                    <label>Name</label>
-                    <input
-                        type="text"
-                        placeholder="enter product name"
-                        {...register("name")}
-                    />
-                    <p>{errors.name?.message}</p>
-                </div>
-                <div className="add__product--form-input">
-                    <label>Amount</label>
-                    <input
-                        type="text"
-                        placeholder="kg, litre, pice, number.."
-                        {...register("amount")}
-                    />
-                    <p>{errors.amount?.message}</p>
-                </div>
-                <div className="add__product--form-input">
-                    <label>Price</label>
-                    <input
-                        type="number"
-                        placeholder="enter product price"
-                        {...register("price")}
-                    />
-                    <p>{errors.price?.message}</p>
-                </div>
-                <div className="add__product--form-input">
-                    <label>Brand Name</label>
-                    <input
-                        type="text"
-                        placeholder="enter product brand name"
-                        {...register("brand")}
-                    />
-                    <p>{errors.brand?.message}</p>
-                </div>
-                <div className="add__product--form-input">
-                    <label>Description</label>
-                    <textarea
-                        type="text"
-                        placeholder="enter product name"
-                        rows="8"
-                        {...register("description")}
-                    />
-                    <p>{errors.description?.message}</p>
-                </div>
-                <button type="submit">SAVE PRODUCT</button>
-            </form>
-            <h3>ADD, UPDATE AND DELETE OPERATION IS MUTED</h3>
+  return (
+    <div className="add__product">
+      <Link to="/products" className="add__product--back">
+        &#8592;Back
+      </Link>
+      <h1 className="add__product--header">{props.headline}</h1>
+      <form
+        className="add__product--form"
+        onSubmit={handleSubmit(formSubmitHandler)}
+      >
+        <div className="add__product--form-input">
+          <label>Main Category</label>
+          <select {...register("mainCategory")} onChange={mainCategoryHandler}>
+            <option value="" hidden>
+              - select one -
+            </option>
+            {categoryList.map((c) => (
+              <option key={c.mCat} value={c.mCat}>
+                {c.mCat}
+              </option>
+            ))}
+          </select>
+          <p>{errors.mainCategory?.message}</p>
         </div>
-    );
+        <div className="add__product--form-input">
+          <label>Sub Category</label>
+          <select {...register("subCategory")}>
+            {singleProd ? (
+              <option value={singleProd.subCategory} hidden>
+                {singleProd.subCategory}
+              </option>
+            ) : (
+              <option value="" hidden>
+                - select one -
+              </option>
+            )}
+            {copySubCate.map((s) => (
+              <option key={s} value={s}>
+                {s}
+              </option>
+            ))}
+          </select>
+          <p>{errors.subCategory?.message}</p>
+        </div>
+        <div className="add__product--form-input">
+          <label>Image</label>
+          <input type="file" accept="image/*" {...register("image")} />
+          <p>{errors.image?.message}</p>
+        </div>
+        <div className="add__product--form-input">
+          <label>Name</label>
+          <input
+            type="text"
+            placeholder="enter product name"
+            {...register("name")}
+          />
+          <p>{errors.name?.message}</p>
+        </div>
+        <div className="add__product--form-input">
+          <label>Amount</label>
+          <input
+            type="text"
+            placeholder="kg, litre, pice, number.."
+            {...register("amount")}
+          />
+          <p>{errors.amount?.message}</p>
+        </div>
+        <div className="add__product--form-input">
+          <label>Price</label>
+          <input
+            type="number"
+            placeholder="enter product price"
+            {...register("price")}
+          />
+          <p>{errors.price?.message}</p>
+        </div>
+        <div className="add__product--form-input">
+          <label>Brand Name</label>
+          <input
+            type="text"
+            placeholder="enter product brand name"
+            {...register("brand")}
+          />
+          <p>{errors.brand?.message}</p>
+        </div>
+        <div className="add__product--form-input">
+          <label>Description</label>
+          <textarea
+            type="text"
+            placeholder="enter product name"
+            rows="8"
+            {...register("description")}
+          />
+          <p>{errors.description?.message}</p>
+        </div>
+        <button type="submit">SAVE PRODUCT</button>
+      </form>
+      <h3>ADD, UPDATE AND DELETE OPERATION IS MUTED</h3>
+    </div>
+  );
 };
 
 const mapDispatchToProps = (dispatch) => {
-    return {
-        onAddProduct: (prodData) => dispatch(actions.addProduct(prodData)),
-        onUpdateProduct: (pId, prod) =>
-            dispatch(actions.updateProduct(pId, prod)),
-        onFetchAllProducts: () => dispatch(actions.fetchAllProducts()),
-    };
+  return {
+    onAddProduct: (prodData) => dispatch(actions.addProduct(prodData)),
+    onUpdateProduct: (pId, prod) => dispatch(actions.updateProduct(pId, prod)),
+    onFetchAllProducts: () => dispatch(actions.fetchAllProducts()),
+  };
 };
 
 export default connect(null, mapDispatchToProps)(ProductFrom);
--- userfrontend/src/store/actions/Auth.js
+++ userfrontend/src/store/actions/Auth.js
@@ -2,136 +2,134 @@ import * as actionTypes from "./actionTypes";
 import axios from "../../axios";
 
 export const userInit = () => {
-    return {
-        type: actionTypes.USER_INIT,
-    };
+  return {
+    type: actionTypes.USER_INIT,
+  };
 };
 
 export const userSignupStart = () => {
-    return {
-        type: actionTypes.USER_SIGNUP_START,
-    };
+  return {
+    type: actionTypes.USER_SIGNUP_START,
+  };
 };
 export const userSignupSuccess = () => {
-    return {
-        type: actionTypes.USER_SIGNUP_SUCCESS,
-    };
+  return {
+    type: actionTypes.USER_SIGNUP_SUCCESS,
+  };
 };
 export const userSignupFail = (err) => {
-    return {
-        type: actionTypes.USER_SIGNUP_FAIL,
-        error: err,
-    };
+  return {
+    type: actionTypes.USER_SIGNUP_FAIL,
+    error: err,
+  };
 };
 
 export const userSignup = (userData) => {
-    return async (dispatch) => {
-        dispatch(userSignupStart());
-        try {
-            axios.post("/buyer/signup", userData);
-            dispatch(userSignupSuccess());
-        } catch (err) {
-            dispatch(userSignupFail(err));
-        }
-    };
+  return async (dispatch) => {
+    dispatch(userSignupStart());
+    try {
+      axios.post("/buyer/signup", userData);
+      dispatch(userSignupSuccess());
+    } catch (err) {
+      dispatch(userSignupFail(err));
+    }
+  };
 };
 
 export const userLoginStart = () => {
-    return {
-        type: actionTypes.USER_LOGIN_START,
-    };
+  return {
+    type: actionTypes.USER_LOGIN_START,
+  };
 };
 
 export const userLoginSuccess = (email, user, token, userId) => {
-    return {
-        type: actionTypes.USER_LOGIN_SUCCESS,
-        email: email,
-        user: user,
-        token: token,
-        userId: userId,
-    };
+  return {
+    type: actionTypes.USER_LOGIN_SUCCESS,
+    email: email,
+    user: user,
+    token: token,
+    userId: userId,
+  };
 };
 export const userLoginFail = (err) => {
-    return {
-        type: actionTypes.USER_LOGIN_FAIL,
-        error: err,
-    };
+  return {
+    type: actionTypes.USER_LOGIN_FAIL,
+    error: err,
+  };
 };
 
 export const userLogin = (userData) => {
-    return async (dispatch) => {
-        dispatch(userLoginStart());
-        try {
-            let result = await axios.post("/buyer/login", userData);
-            const expirationDate = new Date(
-                new Date().getTime() + 365 * 24 * 3600 * 1000
-            );
-            localStorage.setItem("token", result.data.token);
-            localStorage.setItem("expirationDate", expirationDate);
-            localStorage.setItem("userId", result.data.userId);
-            localStorage.setItem("user", result.data.user);
-            localStorage.setItem("email", result.data.email);
-            dispatch(
-                userLoginSuccess(
-                    result.data.email,
-                    result.data.user,
-                    result.data.token,
-                    result.data.userId
-                )
-            );
-            dispatch(checkAuthTimeout(365 * 24 * 3600));
-        } catch (err) {
-            dispatch(userLoginFail(err));
-        }
-    };
+  return async (dispatch) => {
+    dispatch(userLoginStart());
+    try {
+      let result = await axios.post("/buyer/login", userData);
+      const expirationDate = new Date(
+        new Date().getTime() + 365 * 24 * 3600 * 1000
+      );
+      localStorage.setItem("token", result.data.token);
+      localStorage.setItem("expirationDate", expirationDate);
+      localStorage.setItem("userId", result.data.userId);
+      localStorage.setItem("user", result.data.user);
+      localStorage.setItem("email", result.data.email);
+      dispatch(
+        userLoginSuccess(
+          result.data.email,
+          result.data.user,
+          result.data.token,
+          result.data.userId
+        )
+      );
+      dispatch(checkAuthTimeout(365 * 24 * 3600));
+    } catch (err) {
+      dispatch(userLoginFail(err));
+    }
+  };
 };
 
 export const logout = () => {
-    localStorage.removeItem("token");
-    localStorage.removeItem("expirationDate");
-    localStorage.removeItem("userId");
-    return {
-        type: actionTypes.USER_LOGOUT,
-    };
+  localStorage.removeItem("token");
+  localStorage.removeItem("expirationDate");
+  localStorage.removeItem("userId");
+  return {
+    type: actionTypes.USER_LOGOUT,
+  };
 };
 
 export const checkAuthTimeout = (expirationTime) => {
-    return (dispatch) => {
-        setTimeout(() => {
-            dispatch(logout());
-        }, expirationTime * 1000);
-    };
+  return (dispatch) => {
+    setTimeout(() => {
+      dispatch(logout());
+    }, expirationTime * 1000);
+  };
 };
 
 export const autoLogin = () => {
-    return (dispatch) => {
-        const token = localStorage.getItem("token");
-        if (!token) {
-            dispatch(logout());
-        } else {
-            const expirationDate = new Date(
-                localStorage.getItem("expirationDate")
-            );
-            if (expirationDate <= new Date()) {
-                dispatch(logout());
-            } else {
-                const userId = localStorage.getItem("userId");
-                const user = localStorage.getItem("user");
-                const email = localStorage.getItem("email");
-                dispatch(userLoginSuccess(email, user, token, userId));
-                dispatch(
-                    checkAuthTimeout(
-                        (expirationDate.getTime() - new Date().getTime()) / 1000
-                    )
-                );
-            }
-        }
-    };
+  return (dispatch) => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      dispatch(logout());
+    } else {
+      const expirationDate = new Date(localStorage.getItem("expirationDate"));
+      if (expirationDate <= new Date()) {
+        dispatch(logout());
+      } else {
+        const userId = localStorage.getItem("userId");
+        const user = localStorage.getItem("user");
+        const email = localStorage.getItem("email");
+        dispatch(userLoginSuccess(email, user, token, userId));
+        dispatch(
+          checkAuthTimeout(
+            (expirationDate.getTime() - new Date().getTime()) / 1000
+          )
+        );
+      }
+    }
+  };
 };
 
 export const authRedirectPath = (path) => {
-    return {
-        type: actionTypes.AUTH_REDIRECT_PATH,
-        path: path,
-    };
+  return {
+    type: actionTypes.AUTH_REDIRECT_PATH,
+    path: path,
+  };
 };
